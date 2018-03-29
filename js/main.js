a2d033407646dd5c791dba659c2cd626d01ab3f6
Kate744 <kate744@ya.ru>
2018-03-29 12:40:24 +0300

    минимизация файлов

--- js/main.js
+++ js/main.js
@@ -6,27 +6,34 @@ var form = modal.querySelector("form");
 var dates = modal.querySelector("[type=text]");
 var people = modal.querySelector("[type=number]");
 
-link.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  modal.classList.remove("hid");
-  modal.classList.add("modal-show");
-  });
+modal.classList.add("hid");
 
-  form.addEventListener("submit", function (evt) {
-      evt.preventDefault();
-      if (!dates.value || !people.value) {
-      evt.preventDefault();
-      modal.classList.add("modal-error");
-      popup.offsetWidth = popup.offsetWidth;
+link.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    if (modal.classList.contains("hid")) {
+        modal.classList.remove("hid");
+        modal.classList.add("modal-show");
+    } else if (modal.classList.contains("modal-show")) {
+        modal.classList.remove("modal-show");
+        modal.classList.add("hid");
     }
-    });
+});
 
-    window.addEventListener("keydown", function (evt) {
-        if (evt.keyCode === 27) {
-          evt.preventDefault();
-          if (modal.classList.contains("modal-show")) {
+form.addEventListener("submit", function(evt) {
+    evt.preventDefault();
+    if (!dates.value || !people.value) {
+        evt.preventDefault();
+        modal.classList.add("modal-error");
+        popup.offsetWidth = popup.offsetWidth;
+    }
+});
+
+window.addEventListener("keydown", function(evt) {
+    if (evt.keyCode === 27) {
+        evt.preventDefault();
+        if (modal.classList.contains("modal-show")) {
             modal.classList.add("hid");
             modal.classList.remove("modal-error");
-          }
         }
-      });
+    }
+});
